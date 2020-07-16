1019a9a1476445d634691a46c4a059341d0bc3df
Kavinkumar <kavinkumar@mallow-tech.com>
2020-07-16 07:18:46 +0530

    Initial Commit

---
+++ Clocks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clocks
+//
+//  Created by Kavinkumar Veerakumar on 16/07/20.
+//  Copyright © 2020 Kavinkumar Veerakumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
